6f925182b06a47e403c57d8a9cec00ede27daf44
SeiWonPark <psw7347@gmail.com>
2022-05-01 23:10:28 +0900

    chore: removed node js server codes

--- api/src/controllers/market.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import { abis } from '../../../contract/abi';
-
-export const market = (req: Request, res: Response) => {
-  res.send({ result: abis.MARKET_ABI });
-};
--- api/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { market } from '../controllers/market.controller';
-import { nft } from '../controllers/nft.controller';
-
-export const router = Router();
-
-router.get('/nft', nft);
-router.get('/market', market);
