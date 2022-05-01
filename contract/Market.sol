445b824b0203e4e1bde97ea0bae09ae1b1ba14b2
SeiWonPark <psw7347@gmail.com>
2022-05-01 13:25:07 +0900

    feat: solidity nft auction contract

---
+++ contract/Market.sol
@@ -0,0 +1,137 @@
+/**
+ * This is a part of team project of 2022 KMU CS course, 'Practice in Network' by Prof. Sanghwan Lee.
+ * This contract is for configuring NFT auction market and handling its trading process.
+ * Code referred to https://solidity-by-example.org/
+ * Authored by Sooyeon Oh, Taewon Jung and Seiwon Park
+ */
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/// @title ERC-721 standard interface
+/// @notice Uses ERC-721 based token
+/// @dev Well defined at https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+interface ERC721 {
+    function transfer(
+        address from,
+        address to,
+        uint256 nftId
+    ) external;
+}
+
+/// @title NFT market contract
+/// @notice Use this contract for NFT trading
+/// @dev Defined NFT market settings and controllers
+contract Market {
+    /// @dev This emits when message sender bids on an asset
+    /// Indexed sender will be logged so that it makes it easier to filter
+    /// each sender on etherscan.
+    event Bid(address indexed sender, uint256 amount);
+
+    /// @dev This emits when message sender withdraws from the bidded contract
+    /// Indexed bidder will be logged so that it makes it easier to filter
+    /// each sender on etherscan.
+    event Withdraw(address indexed bidder, uint256 amount);
+
+    /// @dev This emits when contract owner ends auction or when the time is up
+    /// Bid winner and bidding amount will be logged.
+    event End(address winner, uint256 amount);
+
+    ERC721 public nft;
+    uint256 public nftId;
+    address payable public seller;
+    uint256 public endAt;
+    bool public started;
+    bool public ended;
+    address public highestBidder;
+    uint256 public highestBid;
+    mapping(address => uint256) public bids;
+
+    /// @notice Configures Market contract with NFT contract
+    /// @dev Initializing Market contract with deployed NFT contract
+    /// @param _nft Deployed NFT address
+    /// @param _nftId Deployed NFT token ID
+    /// @param _startingBid Deployed NFT's start bidding amount
+    constructor(
+        address _nft,
+        uint256 _nftId,
+        uint256 _startingBid
+    ) {
+        nft = ERC721(_nft);
+        nftId = _nftId;
+
+        seller = payable(msg.sender);
+        highestBid = _startingBid;
+    }
+
+    /// @notice Checks if the market is started
+    /// @dev Sets modifiers to give constraints depending on the parameter `_started`
+    /// @param _started State variable `started`
+    /// Throws unless parameter, `_started` is true
+    modifier onStart(bool _started) {
+        require(
+            _started,
+            !_started
+                ? "Auction has already been started."
+                : "Auction is not started yet."
+        );
+        _;
+    }
+
+    /// @notice Starts selling asset on NFT market tagged with its token ID
+    /// @dev Initializing auction market depending on the state variable, `started`
+    /// Throws unless `msg.sender` is the current contract owner
+    function start() external onStart(!started) {
+        require(msg.sender == seller, "not seller");
+
+        nft.transfer(msg.sender, address(this), nftId);
+        started = true;
+        endAt = block.timestamp + 3600 * 1 seconds;
+    }
+
+    /// @notice Bids on the current NFT asset
+    /// @dev Updates highest bidding info with `msg.value`
+    /// Throws if `block.timestamp` already passed `endAt`
+    /// and bidding amount is lower than `highestBid`
+    function bid() external payable onStart(started) {
+        require(block.timestamp < endAt, "ended");
+        require(msg.value > highestBid, "value < highest");
+
+        if (highestBidder != address(0)) {
+            bids[highestBidder] += highestBid;
+        }
+
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+
+        emit Bid(msg.sender, msg.value);
+    }
+
+    /// @notice Takes back message sender's bidded amount
+    /// @dev Resets stored bidded amount and makes transfer to take it back
+    function withdraw() external {
+        uint256 biddedAmount = bids[msg.sender];
+        bids[msg.sender] = 0;
+        payable(msg.sender).transfer(biddedAmount);
+
+        emit Withdraw(msg.sender, biddedAmount);
+    }
+
+    /// @notice Ends bidding and sets bid winner
+    /// @dev Updates NFT info with the bid winner
+    /// Throws unless current time passes the time, `endAt` and checks if it's ended
+    function end() external onStart(started) {
+        // require(block.timestamp >= endAt, "not ended"); FIXME:
+        require(!ended, "ended");
+
+        ended = true;
+        if (highestBidder != address(0)) {
+            nft.transfer(address(this), highestBidder, nftId);
+            seller.transfer(highestBid);
+        } else {
+            nft.transfer(address(this), seller, nftId);
+        }
+
+        emit End(highestBidder, highestBid);
+    }
+}
---
+++ contract/NFT.sol
@@ -0,0 +1,103 @@
+/**
+ * This is a part of team project of 2022 KMU CS course, 'Practice in Network' by Prof. Sanghwan Lee.
+ * This contract is for configuring NFT auction market and handling its trading process.
+ * Code referred to https://solidity-by-example.org/
+ * Authored by Sooyeon Oh, Taewon Jung and Seiwon Park
+ */
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/// @title NFT configuration
+/// @notice Use this contract for NFT configuration
+/// @dev Defined NFT configuration based on ERC-721 standard
+contract NFT {
+    /// @dev This emits when message sender is approved of NFT ID
+    /// Indexed approved owner(message sender) will be logged along with NFT ID
+    /// so that it makes it easier to filter each owner on etherscan.
+    event Approval(
+        address indexed owner,
+        address indexed approved,
+        uint256 indexed nftId
+    );
+
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => address) private _tokenApprovals;
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    /// @notice Returns the number of tokens `owner` has
+    /// @dev Fetches owner from stored state variable, `_owners`
+    /// @param owner Given approved address
+    /// Throws unless given parameter `owner` is specified.
+    function balanceOf(address owner) external view returns (uint256) {
+        require(owner != address(0), "No address specified: zero address");
+        return _balances[owner];
+    }
+
+    /// @notice Gets the owner of NFT from its token ID
+    /// @dev Fetches owner from stored state variable, `_owners`
+    /// @param nftId NFT token ID
+    /// Throws unless given parameter `owner` is specified or approved.
+    function ownerOf(uint256 nftId) public view returns (address owner) {
+        owner = _owners[nftId];
+        require(owner != address(0), "Token doesn't exist");
+    }
+
+    function _transfer(
+        address owner,
+        address from,
+        address to,
+        uint256 nftId
+    ) private {
+        require(from == owner, "Not owner");
+        require(to != address(0), "Transferred to the zero address");
+
+        _approve(address(0), nftId);
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[nftId] = to;
+    }
+
+    function transfer(
+        address from,
+        address to,
+        uint256 nftId
+    ) external {
+        address owner = ownerOf(nftId);
+        _transfer(owner, from, to, nftId);
+    }
+
+    /// @notice Confirms NFT token ID
+    /// @dev Handles storing approved NFT token ID to `_tokenApprovals`
+    function _approve(address to, uint256 nftId) private {
+        _tokenApprovals[nftId] = to;
+    }
+
+    /// @notice Confirms approved owner
+    /// @dev Approves minted owner and athorize the message sender
+    /// @param to Address to approve
+    /// @param nftId NFT token ID
+    /// Throws unless `msg.sender` is approved.
+    function approve(address to, uint256 nftId) external {
+        address owner = _owners[nftId];
+        require(
+            msg.sender == owner || _operatorApprovals[owner][msg.sender],
+            "Not approved owner"
+        );
+        _approve(to, nftId);
+    }
+
+    /// @notice Minting address with its NFT token ID
+    /// @dev Store address as minter
+    /// @param to Address to mint
+    /// @param nftId NFT token ID
+    /// Throws unless given parameter `owner` is specified or already minted.
+    function mint(address to, uint256 nftId) external {
+        require(to != address(0), "Minted to zero address");
+        require(_owners[nftId] == address(0), "Token already minted");
+
+        _balances[to] += 1;
+        _owners[nftId] = to;
+    }
+}
